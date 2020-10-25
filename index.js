e6a27c0ddd477cad02ebf3e240b099ff8dc268d5
burshtei <menachem.burshtein@intel.com>
2020-10-25 22:36:49 +0200

    use cmd

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const core = require('@actions/core');
-const fs = require('fs-extra');
+const fs = require('fs');
 
 const src = core.getInput('src', {required: true})
 const dest = core.getInput('dest', {required: true})
 
 if (fs.existsSync(src)) {
 
-    fs.move(src, dest, function (err) {
-        if (err) throw err
-        console.log('Successfully renamed - AKA moved!')
-    })
-} else {
-    console.log('Src files not exists - no move is needed.')
+   exec(`move ${src} ${dest}`, (error, stdout, stderr) => {
+    if (error) {
+        throw(error.message);
+    }
+    if (stderr) {
+       throw(error.message);
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
 }
