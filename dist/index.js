0d23d9d299ae1c20e06541ec9f210861bf4c38b5
burshtei <menachem.burshtein@intel.com>
2020-10-25 13:41:54 +0200

    'test'

--- dist/index.js
+++ dist/index.js
@@ -6,35 +6,20 @@ module.exports =
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
 const core = __webpack_require__(442);
-const fs = __webpack_require__(747);
+const github = __webpack_require__(671);
 
-const src = core.getInput('src', {required: true})
-const dest = core.getInput('dest', {required: true})
-
-if (existsSync(src)) {
-
-    fs.rename(src, dest, function (err) {
-        if (err) throw err
-        console.log('Successfully renamed - AKA moved!')
-    })
-} else {
-    console.log('Src files not exists - no move is needed.')
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
 }
-
-
-//
-// try {
-//     // `who-to-greet` input defined in action metadata file
-//     const nameToGreet = core.getInput('who-to-greet');
-//     console.log(`Hello ${nameToGreet}!`);
-//     const time = (new Date()).toTimeString();
-//     core.setOutput("time", time);
-//     // Get the JSON webhook payload for the event that triggered the workflow
-//     const payload = JSON.stringify(github.context.payload, undefined, 2)
-//     console.log(`The event payload: ${payload}`);
-// } catch (error) {
-//     core.setFailed(error.message);
-// }
 
 /***/ }),
 
@@ -46,11 +31,11 @@ module.exports = eval("require")("@actions/core");
 
 /***/ }),
 
-/***/ 747:
+/***/ 671:
 /***/ ((module) => {
 
-"use strict";
-module.exports = require("fs");
+module.exports = eval("require")("@actions/github");
+
 
 /***/ })
 
--- index.js
+++ index.js
@@ -1,30 +1,15 @@
 const core = require('@actions/core');
-const fs = require('fs');
+const github = require('@actions/github');
 
-const src = core.getInput('src', {required: true})
-const dest = core.getInput('dest', {required: true})
-
-if (existsSync(src)) {
-
-    fs.rename(src, dest, function (err) {
-        if (err) throw err
-        console.log('Successfully renamed - AKA moved!')
-    })
-} else {
-    console.log('Src files not exists - no move is needed.')
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
 }
-
-
-//
-// try {
-//     // `who-to-greet` input defined in action metadata file
-//     const nameToGreet = core.getInput('who-to-greet');
-//     console.log(`Hello ${nameToGreet}!`);
-//     const time = (new Date()).toTimeString();
-//     core.setOutput("time", time);
-//     // Get the JSON webhook payload for the event that triggered the workflow
-//     const payload = JSON.stringify(github.context.payload, undefined, 2)
-//     console.log(`The event payload: ${payload}`);
-// } catch (error) {
-//     core.setFailed(error.message);
-// }
