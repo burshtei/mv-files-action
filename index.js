de5e1f1164f9d1bb54549326b6fd938acd1650cb
burshtei <menachem.burshtein@intel.com>
2020-10-25 14:42:46 +0200

    fix2

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const core = require('@actions/core');
-const fs = require('fs');
+const fs = require('fs-extra');
 
 const src = core.getInput('src', {required: true})
 const dest = core.getInput('dest', {required: true})
 
 if (fs.existsSync(src)) {
 
-    fs.rename(src, dest, function (err) {
+    fs.move(src, dest, function (err) {
         if (err) throw err
         console.log('Successfully renamed - AKA moved!')
     })
